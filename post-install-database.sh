dac307fa6b2ad3a6bed6d5e36dbc2b10ffee859d
root <root@prod-east1-pbx1-c.us-east1-c.c.vonix-prod.internal>
2019-06-28 18:38:28 +0000

    Split up post-install script to facilitate setting up failover
    We don't need to initialize the database on the failover node, just
    need to create the links in the filesystem script.

--- post-install-database.sh
+++ post-install-database.sh
@@ -21,33 +21,6 @@ service php7.2-fpm stop
 service memcached stop
 service freeswitch stop
 
-# First, move /etc configuration over
-mkdir -p /data/etc
-
-mv /etc/fusionpbx /data/etc
-ln -s /data/etc/fusionpbx /etc/fusionpbx
-
-mv /etc/freeswitch /data/etc
-ln -s /data/etc/freeswitch /etc/freeswitch
-
-mv /etc/fail2ban /data/etc
-ln -s /data/etc/fail2ban /etc/fail2ban
-
-mkdir -p /data/var/lib
-
-mv /var/lib/freeswitch /data/var/lib
-ln -s /data/var/lib/freeswitch /var/lib/freeswitch
-
-mkdir -p /data/usr/share
-
-mv /usr/share/freeswitch /data/usr/share
-ln -s /data/usr/share/freeswitch /usr/share/freeswitch
-
-mkdir -p /data/var/www
-
-mv /var/www/fusionpbx /data/var/www
-ln -s /data/var/www/fusionpbx/ /var/www/fusionpbx
-
 # Configure /etc/fusionpbx/config.lua
 cp /etc/fusionpbx/config.lua /etc/fusionpbx/config.lua.orig
 sed -i 's%database.system = .*%database.system = "'"pgsql://hostaddr=${FUSIONPBX_DB_HOST} port=${FUSIONPBX_DB_PORT} dbname=${FUSIONPBX_DB_NAME} user=${FUSIONPBX_DB_USER} password=${FUSIONPBX_DB_PASS} options=''"'";%' /etc/fusionpbx/config.lua
