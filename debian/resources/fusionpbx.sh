a271e204f5b75262233d835d84790d0bcbd29d5c
Christian King <cking@vonix.io>
2019-08-23 13:44:20 +0000

    Moved fusionpbx to vonix-networks custom repo

--- debian/resources/fusionpbx.sh
+++ debian/resources/fusionpbx.sh
@@ -17,13 +17,9 @@ apt-get install -y ghostscript libtiff5-dev libtiff-tools at
 #get the branch
 if [ .$system_branch = .'master' ]; then
 	verbose "Using master"
-	branch=""
+	branch="-b vonix-master"
 else
-	system_major=$(git ls-remote --heads https://github.com/fusionpbx/fusionpbx.git | cut -d/ -f 3 | grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f1)
-	system_minor=$(git ls-remote --tags https://github.com/fusionpbx/fusionpbx.git $system_major.* | cut -d/ -f3 |  grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f2)
-	system_version=$system_major.$system_minor
-	verbose "Using version $system_version"
-	branch="-b $system_version"
+	branch="-b vonix-4.4"
 fi
 
 #add the cache directory
@@ -31,6 +27,6 @@ mkdir -p /var/cache/fusionpbx
 chown -R www-data:www-data /var/cache/fusionpbx
 
 #get the source code
-git clone $branch https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+git clone $branch https://github.com/vonix-networks/fusionpbx.git /var/www/fusionpbx
 chown -R www-data:www-data /var/www/fusionpbx
 chmod -R 755 /var/www/fusionpbx/secure
